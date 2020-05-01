28b760dcc5a19d97e348da24254cd9dcbc5cbbee
Jaime <jaimelopezav@gmail.com>
2020-05-01 09:58:14 -0700

    adds credits to Jahhein

--- airpods.1m.sh
+++ airpods.1m.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Airpods.sh
-# Output connected Airpods battery levels via CLI
+# Output connected Airpods battery levels to BitBar
+#
+# Modified script from https://gist.github.com/Jahhein/45b8e8c9c36a0932189a5037f990bcdd
 
-AIRPOD_ICON=$'\uF7CC';
+AIRPOD_ICON=$'🎧';
 
 BATTERY_INFO=(
   "BatteryPercentCombined" 
@@ -24,7 +26,7 @@ if [[ "${CONNECTED}" ]]; then
     declare -x "${info}"="$(awk -v pat="${info}" '$0~pat{gsub (";",""); print $3 }'<<<"${BT_DATA}")"
     [[ ! -z "${!info}" ]] && OUTPUT="${OUTPUT} $(awk '/BatteryPercent/{print substr($0,15)": "}'<<<"${info}")${!info}%"
   done
-   printf "%s\\n" "🎧 ${OUTPUT}"
+   printf "%s\\n" "${AIRPOD_ICON} ${OUTPUT}"
 fi
 
 exit 0
